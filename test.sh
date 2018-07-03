35df799009791f8156c241fe996dc09fe852c90d
PomDziad <40808288+PomDziad@users.noreply.github.com>
2018-07-03 23:33:02 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #To po prostu test, bo może sobie zrobię skrypt do instalacji Archa na szybkości. Zawsze jakieś nowe doświadczenie.
 #Dlatego nie polecam.
-dialog --defaultno --title "DON'T BE A BRAINLET!" --yesno "This is an Arch install script that is very rough around the edges.\n\nOnly run this script if you're a big-brane who doesn't mind deleting your entire /dev/sda drive.\n\nThis script is only really for me so I can autoinstall Arch.\n\nt. Luke"  15 60 || exit
