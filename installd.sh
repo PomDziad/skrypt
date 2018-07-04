61a60ef35091568478f44dffdcb1c831b5ce6ae2
PomDziad <40808288+PomDziad@users.noreply.github.com>
2018-07-04 15:10:03 +0200

    Update installd.sh

--- installd.sh
+++ installd.sh
@@ -11,27 +11,27 @@ echo "LANG=pl_PL.UTF-8" > /etc/locale.conf &&
 #Nazwa kompa
 echo "ArchLinux" > /etc/hostname &&
 
-#Hasło admina
-echo "Wpisz hasło dla konta root"
-passwd &&
-
 #bootloader (grub-install --target=x86_64-efi --efi-directory=esp --bootloader-id=GRUB dla EFI)
 pacman -S grub &&
 grub-install --target=i386-pc /dev/sda &&
 grub-mkconfig -o /boot/grub/grub.cfg &&
-
-#dodawanie użytkownika (sudo ręcznie)
-echo "Wpisz nazwę użytkownika do stworzenia (małe litery): "
-read user
-useradd --create-home $user &&
-passwd $user &&
 
 #podstawy środowiska
 pacman -S xorg lightdm lightdm-gtk-greeter xf86-video-vesa ttf-dejavu lxde leafpad sudo &&
 
 #Ustawianie greetera
 echo "[Seat:*]
-greeter-session=lightdm-gtk-greeter" >> /etc/lightdm/lightdm.conf.d &&
+greeter-session=lightdm-gtk-greeter" > /etc/lightdm/lightdm.conf.d
+
+#Hasło admina
+echo "Wpisz hasło dla konta root"
+passwd &&
+
+#dodawanie użytkownika (sudo ręcznie)
+echo "Wpisz nazwę użytkownika do stworzenia (małe litery): "
+read user
+useradd --create-home $user &&
+passwd $user &&
 
 #włączanie lightdm
 systemctl enable lightdm && echo "Teraz zresetuj"
