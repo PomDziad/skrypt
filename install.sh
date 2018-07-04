96ce7205a949bcf03d337bb7a36d208e2962e7a2
PomDziad <40808288+PomDziad@users.noreply.github.com>
2018-07-04 12:28:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,49 +10,12 @@ case $zgoda in
 esac
 
 #Instalacja podstawy
-pacstrap /mnt base
+pacstrap /mnt base &&
 
 #FSTAB
-genfstab -U /mnt >> /mnt/etc/fstab
+genfstab -U /mnt >> /mnt/etc/fstab &&
 
 #chrootowanie
-arch-chroot /mnt
-
-#Czas
-ln -sf /usr/share/zoneinfo/Europe/Warsaw /etc/localtime && hwclock --systohc &&
-
-#Język
-echo "pl_PL.UTF-8 UTF-8" >> /etc/locale.gen && locale-gen &&
-echo "LANG=pl_PL.UTF-8" > /etc/locale.conf &&
-
-#Nazwa kompa
-echo "ArchLinux" > /etc/hostname &&
-
-#Hasło admina
-echo "Wpisz hasło dla konta root"
-passwd &&
-
-#bootloader (grub-install --target=x86_64-efi --efi-directory=esp --bootloader-id=GRUB dla EFI)
-pacman -S grub &&
-grub-install --target=i386-pc /dev/sda &&
-grub-mkconfig -o /boot/grub/grub.cfg &&
-
-#dodawanie użytkownika (sudo ręcznie)
-echo "Wpisz nazwę użytkownika do stworzenia (małe litery): "
-read user
-useradd --create-home $user &&
-passwd $user &&
-
-#podstawy środowiska
-pacman -S xorg lightdm ligthdm-gtk-greeter xf86-video-vesa ttf-dejavu lxde leafpad sudo &&
-
-#Ustawianie greetera
-echo "[Seat:*]
-greeter-session=lightdm-gtk-greeter" >> /etc/lightdm/lightdm.conf.d &&
-
-#włączanie lightdm
-systemctl enable lightdm && echo "Teraz zresetuj"
-
-
-
+wget installd.sh > /mnt/dalej.sh && arch-chroot /mnt
+bash dalej.sh
 
