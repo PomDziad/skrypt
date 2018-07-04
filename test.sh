dd2a7c1f2c2ac6c88a6775c2c97a368471186f1b
PomDziad <40808288+PomDziad@users.noreply.github.com>
2018-07-04 02:22:12 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+#Czcionka
+setfont Lat2-Terminus16.psfu.gz -m 8859-2 &&
+echo -n "Ten skrypt ma za zadanie uruchomienie 
+środowiska graficznego LXDE na Arch Linuxie 
+bez jego instalacji. Aby kontynuować, wpisz 'tak'.
+"
+read zgoda
+case $zgoda in
+[tak]* )
 #Rozszerzanie dostępnego miejsca na USB
 mount -o remount,size=2G /run/archiso/cowspace &&
 #Wgrywanie niezbędnych pakietów i późniejsze usunięcie pozostałości
@@ -7,13 +16,24 @@ pacman -Scc --noconfirm &&
 #Ustawianie greetera
 echo "[Seat:*]
 greeter-session=lightdm-gtk-greeter" >> /etc/lightdm/lightdm.conf.d &&
-#Hasło admina, w razie czego
-echo "teraz dodajesz haslo admina" && passwd &&
 #Dorabianie pliku na pulpit
 echo "Jeżeli chcesz dodać użytkownika osobnego, to masz instrukcję:
 1. Dodaj usera: useradd --create-home <nazwa usera>
 2. Dograj sudo: pacman -S sudo
 3. Otwórz plik: visudo /etc/sudoers
-4. Pod root ALL=(ALL) ALL dodaj takie samo, tylko z nazwą użytkownika" >> README!!!.txt &&
+4. Pod root ALL=(ALL) ALL dodaj takie samo, tylko z nazwą użytkownika" >> ./Desktop/README.txt &&
+esac
+echo -n "Za chwilę powinien uruchomić sie ekran logowania. 
+Użytkownik to root, a hasła nie ma. 
+Możesz dodać hasło dla konta root 
+za pomocą komendy passwd.
+W pliku README jest info o dodawaniu
+zwykłego użytkownika. 
+Wpisz 'tak', aby kontynuować.
+"
+read zgoda1
+[tak]* )
 #Wystartowanie menadżera logowania
-systemctl start lightdm
+systemctl start lightdm;
+esac
+
